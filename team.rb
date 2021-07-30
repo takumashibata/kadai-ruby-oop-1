bce2ea237af696b1b25fd866420ecdcea9f09518
takumashibata <westside_storydoor@yahoo.co.jp>
2021-07-30 15:37:08 +0000

    team.rb commit

---
+++ team.rb
@@ -0,0 +1,36 @@
+class Team
+    
+    attr_accessor :name, :win, :lose, :draw
+    
+    def initialize(name, win, lose, draw)
+        self.name = name
+        self.win = win
+        self.lose = lose
+        self.draw = draw
+    end
+    
+    def calc_win_rate
+        return win.to_f / ( win.to_f + lose.to_f)
+    end
+    
+    def show_team_result
+        puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}負 #{self.draw}分 勝率は #{calc_win_rate} です"
+    end
+    
+end
+
+Giants = Team.new("Giants", 67, 45, 8)
+Tigers = Team.new("Tigers", 60, 53, 7)
+Dragons = Team.new("Dragons", 60, 55, 5)
+BayStars = Team.new("BayStars", 56, 58, 6)
+Carp = Team.new("Carp", 52, 56, 12)
+Swallows = Team.new("Swallows", 41, 69, 10)
+
+
+puts "チーム 試合　勝利　敗北　引分" 
+Giants.show_team_result
+Tigers.show_team_result
+Dragons.show_team_result
+BayStars.show_team_result
+Carp.show_team_result
+Swallows.show_team_result
